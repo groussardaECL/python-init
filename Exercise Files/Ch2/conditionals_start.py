08e795f8c0375baa5f695534bf5099911def576e
Arnaud GROUSSARD <groussarda@gmail.com>
2016-11-18 12:15:58 +0100

    Ends first session on this course

--- Exercise Files/Ch2/conditionals_start.py
+++ Exercise Files/Ch2/conditionals_start.py
@@ -3,7 +3,12 @@
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
 
 def main():
-  x, y = 10, 100
+  x, y = 100, 100
+
+
+# conditional statements let you use "a if C else b"
+  st = "x is less than y" if (x < y) else "x is greater than or equal to y"
+  print (st)
   
   
 if __name__ == "__main__":
